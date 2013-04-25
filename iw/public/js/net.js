d8418d84c2475a760699627ea6f6036844d97183
pwned <pwned@pwned.(none)>
2013-04-25 16:13:57 +0200

    Modify the net.js for recover forms' data of templates html

--- iw/public/js/net.js
+++ iw/public/js/net.js
@@ -26,9 +26,32 @@ $(window).load((function()
                         return cookieValue;
                     }
 
-                    $("a.menu").on("click", function()
+                   function Form(e)
+                   {
+                        e.preventDefault();
+                        $.ajax({
+                            type: 'post',
+                            headers:
+                                {
+                                  "X-CSRFToken": csrftoken
+                                },
+                                data:$(this).serialize(),
+                                url: '/ajaxform/'+id,
+                                timeout: 3000,
+                                success:function(data)
+                                        {
+                                            $("info").html(data)
+                                        },
+                                error:function()
+                                      {
+                                          alert("La requête n'a pas abouti")
+                                      }
+                                });
+                   }
+
+                    $("a.menu").on("click", function(e)
                                                {
-                                                   var id = this.id
+                                                   id = this.id
                                                    $.ajax({
                                                        type: 'post',
                                                        headers:
@@ -41,13 +64,17 @@ $(window).load((function()
                                                         },
                                                        url: '/control/',
                                                        timeout: 3000,
-                                                       success:function(data)
+                                                       success:function(data, e)
                                                                {
                                                                    $("#dispopt").html(data);
                                                                    $("#option").modal("show");
-                                                                   if (id == 0)
+                                                                   if (id == 0 || id == 1 || id == 2 || id == 4)
                                                                    {
-                                                                       //$(".collapse").collapse({toggle:true});
+                                                                       $("#form0").on("submit", Form);
+                                                                       if(id == 0 || id== 4)
+                                                                       {
+                                                                           $("#form1").on("submit", Form);
+                                                                       }
                                                                    }
 
                                                                },
@@ -57,10 +84,13 @@ $(window).load((function()
                                                                }
                                                    })
                                                });
+
                     $(window).on("resize", function()
                                             {
                                                 $('canvas').attr("width", $(window).width()*0.75);
                                             });
+
+                    var id;
                     var csrftoken = GetCookie('csrftoken');
                     $('canvas').attr("width", $(window).width()*0.75);
                 })());
