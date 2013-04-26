b00ad8611f219f401b0fdabbf8903dcff3711718
pwned <pwned@pwned.(none)>
2013-04-26 16:31:24 +0200

    Add a database sqlite and a model for scan's parameters

--- iw/models.py
+++ iw/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ScanParam(models.Model):
+    id = models.AutoField(primary_key=True)
+    ip = models.BooleanField()
+    mac = models.BooleanField()
+    os = models.BooleanField()
+    hostname = models.BooleanField()
+    type = models.IntegerField()
