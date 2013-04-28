74264ff6df5fe2d043c54d923b3d7e509d6fb51c
pwned <pwned@pwned.(none)>
2013-04-28 17:04:29 +0200

    Modify the method GetIpMac and add methods GetIP ans GetMac

--- scan/LocalScan_nmap.py
+++ scan/LocalScan_nmap.py
@@ -15,14 +15,44 @@ class LocalScan:
     @param - net : this variable contain the pattern of the network (ex : 192.168.0.* or 10.8.12.* for netmask /24)
     @param - name : this variable contain the name for identify the scan in the database
     """
-    def __init__(self, name, net):
+    def __init__(self, name, net, interface):
         self.net = net
         self.name = name
+        self.interface = interface
 
-    def GetIpAndMac(self):
+
+    def GetIpMac(self):
         """
-        GetIpAndMac :
-        @details This method scans the network to get adresses ip and mac of all machines
+        GetIp :
+        @details This method scans the network to get adresses ip of all machines
+        """
+        n = list()
+        conf = os.popen("ifconfig "+self.interface)
+        conf = conf.readlines()
+        myconf = {'ip':((conf[1].split(":"))[1].split(" "))[0], 'mac': ((conf[0].split("HWaddr"))[1].split(" "))[1]}
+        cmd = os.popen("nmap -sP "+self.net)
+        to = cmd.readlines()
+        i = 0
+        while i < len(to):
+            if re.match("Nmap scan report for \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", to[i]):
+                if re.match((to[i].split(" "))[4].replace("\n", ""), myconf["ip"]):
+                    print 'enter'
+                    n.append({"mac": myconf["mac"], "ip": myconf["ip"], "device": None, "os": None, "hostname": None, "route": None})
+                else:
+                    ip = (to[i].split(" "))[4].replace("\n", " ")
+                    i += 2
+                    if re.match("MAC Address: [A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}", to[i]):
+                        mac = (to[i].split(" "))[2]
+                    else:
+                        mac = None
+                    n.append({"mac": mac, "ip": ip, "device": None, "os": None, "hostname": None, "route": None})
+            i += 1
+        return n
+
+    def GetIp(self):
+        """
+        GetIp :
+        @details This method scans the network to get adresses ip of all machines
         """
         n = list()
         cmd = os.popen("nmap -sP "+self.net)
@@ -31,6 +61,15 @@ class LocalScan:
                 ip = elem.split(" ")
                 n.append({"mac": None, "ip": ip[4].replace('\n', ''), "device": None, "os": None, "hostname": None, "route": None})
         return n
+
+    def GetMac(self, ip):
+        cmd = os.popen("arping -c 1 " +ip)
+        for elem in cmd:
+            #\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}
+            if re.match("Unicast reply from "+ip+" \[[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}\]", elem):
+                mac = elem.split(" ")
+                break
+        return mac[4].replace(']', '').replace('[', '')
 
     def GetDevice(self):
         """
@@ -64,6 +103,8 @@ class LocalScan:
 
 if __name__ == "__main__":
     #scan = LocalScan('home', "10.8.96.1/20")
-    scan = LocalScan('home', '192.168.0.*')
-    l = scan.GetIpAndMac()
-    print l
+    scan = LocalScan('home', '192.168.0.*', 'eth0')
+    #l = scan.GetIp()
+    #print l
+    #print scan.GetMac(l[0]["ip"])
+    print scan.GetIpMac()
