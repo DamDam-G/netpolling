96d5e2655d290f5f462857ecc1ca7b2c74ae7871
Damien <damdam.gold@gmail.com>
2013-04-29 15:44:25 +0200

    Create scan table for SQLite and change a attributes of the class LocalScan_nmap

--- iw/models.py
+++ iw/models.py
@@ -19,5 +19,35 @@ class Log(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.TextField(max_length=100)
     content = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     type = models.IntegerField()
+
+class Machine(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    device = models.ForeignKey('Device')
+    os = models.ForeignKey('OS')
+
+class Interface(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_machine = models.ForeignKey('Machine')
+    ip = models.IPAddressField()
+    mac = models.TextField(max_length=17)
+    gw = models.ForeignKey('Route')
+
+class Route(models.Model):
+    id = models.AutoField(primary_key=True)
+    gw = models.IPAddressField()
+
+class BandWidth(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_machine = models.ForeignKey('Machine')
+    bw = models.IntegerField()
+
+class OS(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+
+class Device(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
