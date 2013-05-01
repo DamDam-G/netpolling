6367333756e1f15a0ff5b4b1d2fd52f1ce01e45a
pwned <pwned@pwned.(none)>
2013-05-01 09:55:02 +0200

    Delete some some files about the scan in local network and rename LocalScan_nmap for LocalScan

--- scan/LocalScan.py
+++ scan/LocalScan.py
@@ -1,68 +1,172 @@
 #! /usr/bin/python2.7
 # -*- coding:Utf-8 -*-
 from scapy.all import *
-#from scapy.modules.nmap import *
-"""!
-@package docstring
-This file contain the class : LocalScan
-"""
+import os
+import re
 
 class LocalScan:
     """!
-    LocalScan :
-    @param - net : this variable contain the pattern of the network (ex : 192.168.0.* or 10.8.12.* for netmask /24)
+    @author Damien Goldenberg
+    @name LocalScan
+    @brief This class allows to scan a local network
+    @param - net : this variable contain a list with some dictionary who represent machines up (contain ip, mac, hostname, os)
     @param - name : this variable contain the name for identify the scan in the database
+    @param - mask : this variable contain the pattern of the network (ex : 192.168.0.0/24 or 10.8.96.0/20)
+    @param - interface : this variable contain the network interface on the app works
+    @param - gw : this variable contain the gateway of the network
+    @param - route : this variable the path to go to the gateway
+    @version V-2.0
+    @copyright GNU GPL V-3
     """
-    def __init__(self, name, net):
-        self.net = net
-        self.name = name
-
-    def GetIpAndMac(self):
+    def __init__(self, name, mask, interface):
         """!
-        GetIpAndMac :
-        @details This method scans the network to get ip and mac adresses of all machines
+        @author Damien Goldenberg
+        @name __init__
+        @brief This function initializes the object
+        @param - self : this is a variable that represents the current object
+        @param - name : this variable contain the name for identify the scan in the database
+        @param - mask : this variable contain the pattern of the network (ex : 192.168.0.0/24 or 10.8.96.0/20)
+        @param - interface : this variable contain the network interface on the app works
+        @version V-2.0
+        @copyright GNU GPL V-3
         """
-        ans, unans = arping(self.net, timeout=120)
-        l = list()
+        self.net = []
+        self.name = name
+        self.interface = interface
+        self.mask = mask
+        self.gw = ''
+        self.route = []
+        self.GetGW()
+        self.GetRoute()
 
-        for elem in ans:
-            l.append({"mac": elem[1].src, "ip": elem[1].psrc, "device": None, "os": None, "hostname": None, "route": None})
-        return l
+    def GetIpMac(self):
+        """!
+        @author Damien Goldenberg
+        @name GetIp
+        @brief This method scans the network to get addresses ip and mac of all machines up
+        @param - self : this is a variable that represents the current object
+        @version V-1.0
+        @copyright GNU GPL V-3
+        """
+        n = list()
+        conf = os.popen("ifconfig "+self.interface)
+        conf = conf.readlines()
+        myconf = {'ip':((conf[1].split(":"))[1].split(" "))[0], 'mac': ((conf[0].split("HWaddr"))[1].split(" "))[1]}
+        cmd = os.popen("nmap -sP "+self.mask)
+        to = cmd.readlines()
+        i = 0
+        print to
+        while i < len(to):
+            if re.match("Nmap scan report for \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", to[i]):
+                if re.match((to[i].split(" "))[4].replace("\n", ""), myconf["ip"]):
+                    n.append({"mac": myconf["mac"], "ip": myconf["ip"], "device": None, "os": None, "hostname": None})
+                else:
+                    ip = (to[i].split(" "))[4].replace("\n", " ")
+                    i += 2
+                    if re.match("MAC Address: [A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}", to[i]):
+                        mac = (to[i].split(" "))[2]
+                    else:
+                        mac = None
+                    n.append({"mac": mac, "ip": ip, "device": None, "os": None, "hostname": None})
+            i += 1
+        self.net = n
 
     def GetDevice(self):
         """!
-        GetDevice :
+        @author
+        @name GetDevice
+        @brief This method to get the device of a machine
+        @param - self : this is a variable that represents the current object
+        @version V-0
+        @copyright GNU GPL V-3
         """
         return
 
     def GetOS(self, ip):
         """!
-        GetOS :
+        @author Damien Goldenberg
+        @name GetOS
+        @brief This method scans a machine to get the os
+        @param - self : this is a variable that represents the current object
+        @param - ip : this is variable represents the address ip of the machine that would like to scan
+        @version V-0.1
+        @copyright GNU GPL V-3
         """
-        #load_module("nmap")
-        #conf.nmap_base
-        #ans, unans = nmap_fp(ip)
-        #traitement de la réponse pour en resortir un os
-        os = ''
-        return os
+        #nmap -sV --version-all
+        cmd = os.popen("nmap -O "+ip)
+        to = cmd.readlines()
+        i = 0
+        system = "Unknown"
+        while i < len(to):
+            if re.search(".*Linux.*", to[i], re.IGNORECASE):
+                system = "Linux"
+                break
+            if re.search(".*Windows.*", to[i], re.IGNORECASE):
+                system = "Windows"
+                break
+            if re.search(".*Mac.*", to[i], re.IGNORECASE):
+                system = "Mac"
+                break
+            i += 1
+
+        return system
 
     def GetHostName(self):
+        """!
+        @author
+        @name GetHostName
+        @brief This method try to get the hostname of the machine
+        @param - self : this is a variable that represents the current object
+        @version V-0
+        @copyright GNU GPL V-3
         """
-        GetHostName :
-        """
-        # utiliser nslookup /host à test (il faut un servuer dns voir ce que ça donne à l'école
+
         return
+
+    def GetGW(self):
+        """!
+        @author Damien Goldenberg
+        @name GetGW
+        @brief This method get the gateway of the network
+        @param - self : this is a variable that represents the current object
+        @version V-0.1
+        @copyright GNU GPL V-3
+        """
+        cmd = os.popen("route | grep default")
+        cmd = cmd.read()
+        self.gw = list(set(cmd.split(' ')))[3]
 
     def GetRoute(self):
+        """!
+        @author Damien Goldenberg
+        @name GetRoute
+        @brief This method analyze what is the route take of machines in this network
+        @param - self : this is a variable that represents the current object
+        @version V-0.1
+        @copyright GNU GPL V-3
         """
-        GetRoute :
-        """
-        return
+        t = traceroute("8.8.8.8")
+        i = 0
+        path = list()
+        print "gw = "+self.gw
+        while i < len(t):
+            print t[0][i][1].src+" = "+self.gw
+            self.route.append(t[0][i][1].src)
+            if t[0][i][1].src == self.gw:
+                break
+            i += 1
 
 if __name__ == "__main__":
-    scan = LocalScan('home', "10.8.96.1/20")
-    #scan = LocalScan('home', '192.168.0.*')
-    l = scan.GetIpAndMac()
-    #for elem in l:
-     #   elem.os = scan.GetOS(elem.ip)
-    print l
+    scan = LocalScan('home', '10.8.96.0/20', 'eth0')
+    #scan = LocalScan('home', '192.168.0.*', 'eth0')
+    #l = scan.GetIp()
+    #print l
+    #print scan.GetMac(l[0]["ip"])
+    #scan.GetIpMac()
+    #print scan.net
+    #print scan.GetOS("10.8.111.153")
+    #scan.GetGW()
+    #print scan.gw
+    #scan.GetRoute()
+    print "gw = "+scan.gw
+    print scan.route
