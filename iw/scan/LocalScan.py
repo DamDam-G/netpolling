8f665f2a498e1f04030081d8641f1740b3286631
pwned <pwned@pwned.(none)>
2013-06-06 01:27:30 +0200

    Clear some useless line

--- iw/scan/LocalScan.py
+++ iw/scan/LocalScan.py
@@ -3,7 +3,6 @@
 from scapy.all import *
 import os
 import re
-import json
 from Scan import *
 
 class LocalScan(Scan):
@@ -37,37 +36,6 @@ class LocalScan(Scan):
             if re.match("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", cmd[i]):
                     ip = (cmd[i].split("\t"))[0]
                     mac = (cmd[i].split("\t"))[1]
-                    n.append({"mac": mac, "ip": ip, "device": None, "os": None, "hostname": None})
-            i += 1
-        self.net = n
-
-    def GetIpMac2(self):
-        """!
-        @author Damien Goldenberg
-        @name GetIp
-        @brief This method scans the network to get addresses ip and mac of all machines up
-        @param - self : this is a variable that represents the current object
-        @version V-1.0
-        @copyright GNU GPL V-3
-        """
-        n = list()
-        conf = (os.popen("ifconfig "+self.interface)).readlines()
-        #conf = conf.readlines()
-        myconf = {'ip':((conf[1].split(":"))[1].split(" "))[0], 'mac': ((conf[0].split("HWaddr"))[1].split(" "))[1]}
-        cmd = (os.popen("nmap -sP "+self.mask)).readlines()
-        #to = cmd.readlines()
-        i = 0
-        while i < len(cmd):
-            if re.match("Nmap scan report for \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", cmd[i]):
-                if re.match((cmd[i].split(" "))[4].replace("\n", ""), myconf["ip"]):
-                    n.append({"mac": myconf["mac"], "ip": myconf["ip"], "device": None, "os": None, "hostname": None})
-                else:
-                    ip = (cmd[i].split(" "))[4].replace("\n", " ")
-                    i += 2
-                    if re.match("MAC Address: [A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}", cmd[i]):
-                        mac = (cmd[i].split(" "))[2]
-                    else:
-                        mac = None
                     n.append({"mac": mac, "ip": ip, "device": None, "os": None, "hostname": None})
             i += 1
         self.net = n
