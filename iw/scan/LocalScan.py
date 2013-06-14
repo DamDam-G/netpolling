d01c8f76f0f54b4dfc6bdd32bf3e8659ceb08787
damien <damdam.gold@gmail.com>
2013-06-14 04:40:26 -0700

    Add the method GetDevice in the method GetIpMac

--- iw/scan/LocalScan.py
+++ iw/scan/LocalScan.py
@@ -36,7 +36,7 @@ class LocalScan(Scan):
             if re.match("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", cmd[i]):
                     ip = (cmd[i].split("\t"))[0]
                     mac = (cmd[i].split("\t"))[1]
-                    n.append({"mac": mac, "ip": ip, "device": None, "os": None, "hostname": None, "bw": None, "percent": None})
+                    n.append({"mac": mac, "ip": ip, "device": self.GetDevice(mac), "os": None, "hostname": None, "bw": None, "percent": None})
             i += 1
         self.net = n
 
