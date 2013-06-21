85984956289c9318aa839a20582d88365d74416e
Itinet <iti@iti.fr>
2013-06-21 12:00:17 +0200

    Log function

--- iw/log.py
+++ iw/log.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sqlite3 as lite
-import sys
 import conf.netenv as ENV
-
-def PutLog(name, content, type):
-    co = lite.connect(ENV.db+'netpolling.sql')
-    try:
-        with co:
-            cur = co.cursor()
-            print 'toto'
-            cur.execute("INSERT INTO \"netpolling\".\"iw_log\" VALUES('', '"+name+"','"+content+"', '', "+str(type)+")")
-            co.commit()
-    except lite.Error, e:
-        if co:
-            co.rollback()
-        print "Error %s:" % e.args[0]
-        sys.exit(1)
-    finally:
-        if co:
-            co.close()
+def PutLog(name, content, date, type):
+    fd = open("/opt/netpolling/iw/conf/log", "a")
+    content = content.replace(' ','_')
+    name = name.replace(' ','_')
+    list = [name, content, date, str(type)]
+    line = ' '.join(list) + "\n"
+    fd.write(line)
+    fd.close()
+#    co = lite.connect("/opt/netpolling/netpolling.sql")
+#    try:
+#        with co:
+#            cur = co.cursor()
+#            print 'toto'
+#            cur.execute("INSERT INTO \"netpolling\".\"iw_log\" VALUES('', '"+name+"','"+content+"', '', "+str(type)+")")
+#            co.commit()
+#    except lite.Error, e:
+#        if co:
+#            co.rollback()
+#        print "Error %s:" % e.args[0]
+#        sys.exit(1)
+#    finally:
+#        if co:
+#            co.close()
 
 if __name__ == "__main__":
-    PutLog('toto', 'trolololol', 1)
+    PutLog('tota coucou', 'trolololol qui tue des zobs', '01/12/64', 1)
