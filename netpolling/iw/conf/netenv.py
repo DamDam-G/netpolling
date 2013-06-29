69459cbeb3782eb289240fd351d5329e0a6b70d1
damien <damdam.gold@gmail.com>
2013-06-28 22:17:01 -0700

    Change the architecture

---
+++ netpolling/iw/conf/netenv.py
@@ -0,0 +1,13 @@
+"""
+This is an important modul.
+It indicates the environment of the app, if you want to change the architecture or add a path in the code, you must to refer here
+"""
+path = "/opt/netpolling/netpolling/"
+iw = path+"iw"
+scripts = iw+"/scripts/"
+conf = iw+"/conf/"
+templates = iw+"/templates/"
+public = iw+"/public/"
+db = path+"netpolling.sql"
+screen = public+"screenshots/"
+listen = public+"listen/"
