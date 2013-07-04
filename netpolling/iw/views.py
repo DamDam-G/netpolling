71d8619ad390f017e5e5479f67acc5cabb247a41
damien <damdam.gold@gmail.com>
2013-07-03 17:33:13 -0700

    Add security on html form

--- netpolling/iw/views.py
+++ netpolling/iw/views.py
@@ -123,9 +123,6 @@ def Control(request):
                         r.update(name=param.get(name[i], 'name').replace('"', ''))
                         r.update(netmask=param.get(name[i], 'netmask').replace('"', ''))
                         r.update(interface=param.get(name[i], 'interface').replace('"', ''))
-                        r.update(device=int(param.get(name[i], 'device')))
-                        r.update(os=int(param.get(name[i], 'os')))
-                        r.update(hostname=int(param.get(name[i], 'hostname')))
                         i += 1
                 except ConfigParser.Error, err:
                     print 'Oops, une erreur dans votre fichier de conf (%s)' % err
@@ -205,16 +202,10 @@ def AjaxForm(request, id):
             if request.POST.get("type"):
                 if request.POST.get("type") == "local" or request.POST.get("type") == "extern":
                     t = 0 if request.POST.get("type") == "local" else 1
-                    device=1 if request.POST.get("device") else 0
-                    os=1 if request.POST.get("os") else 0
-                    hostname=1 if request.POST.get("hostname") else 0
                     r = ScanParam.objects.filter(type=t)
                     r.update(name=request.POST.get("name"))
                     r.update(netmask=request.POST.get("mask"))
                     r.update(interface=request.POST.get("interface"))
-                    r.update(device=device)
-                    r.update(os=os)
-                    r.update(hostname=hostname)
                     config = ConfigParser.RawConfigParser()
                     config.read(r''+ENV.conf+'netpolling.conf')
                     section = ["LocalScan", "ExternScan"]
@@ -222,9 +213,6 @@ def AjaxForm(request, id):
                         config.set(section[t], 'name', r'"'+request.POST.get("name")+'"')
                         config.set(section[t], 'netmask', r'"'+request.POST.get("mask")+'"')
                         config.set(section[t], 'interface', r'"'+request.POST.get("interface")+'"')
-                        config.set(section[t], 'device', device)
-                        config.set(section[t], 'os', os)
-                        config.set(section[t], 'hostname', hostname)
                         with open(r''+ENV.conf+'netpolling.conf', 'wb') as configfile:
                             config.write(configfile)
                     except ConfigParser.Error, err:
@@ -330,11 +318,7 @@ def Sniff(request):
     if request.is_ajax():
         if request.POST.get("name") and request.POST.get("ip") and request.POST.get("time"):
             os.popen("timeout "+str(request.POST.get("time"))+"s tcpdump -i eth0 host "+request.POST.get("ip")+" -w "+ENV.listen+request.POST.get("name")+"-"+request.POST.get("ip")+".pcap")
-            #a = sniff(filter="host "+str(request.POST.get("ip"))+"", count=1000000, timeout=int(request.POST.get("time")))
-            #wrpcap(ENV.listen+request.POST.get("name")+"-"+request.POST.get("ip")+".pcap", a)
-            #wrpcap("toto.pcap", a)
             return HttpResponse(json.dumps({"success":1, "rep":"<div class=\"alert alert-success\"><a class='btn btn-info' href='/public/listen/"+request.POST.get("name")+"-"+request.POST.get("ip")+".pcap'>DL</a></div>"}))
-            #return HttpResponse("<div class=\"alert alert-success\">L'écoute a été terminé avec succès</div><a class='btn btn-info' href='/public/listen/"+request.POST.get("name")+"-"+request.POST.get("ip")+".pcap'>DL</a>")
         else:
             return HttpResponse(json.dumps({"success":0, "rep":"42, The Big Question of Life, the Universe and Everything.<br /> <div class=\"alert alert-error\">[ERROR] : hum hum ...  I think you must contact your admin system ;)</div>"}))
     else:
