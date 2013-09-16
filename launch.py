454593b3ee74c27fa1b6e6647f23c87ef66f0fe3
damien <damdam.gold@gmail.com>
2013-09-16 03:30:09 -0700

    Add the launch server script

--- launch.py
+++ launch.py
@@ -1,4 +1,21 @@
 import os
 
-ip = input("Tappez l'adresse ip du serveur")
-os.popen("python /opt/netpolling/manage.py runserver "+str(ip)+":4242 >> netpolling.log")
+path = "/opt/netpolling/"
+ip = raw_input("Tappez l'adresse ip du serveur : ")
+print """
+                   |"|                                      #   ___         _     _       _     _       .      .       _   _
+     (((          _|_|_         ()_()          `  ___  '    #  <_*_>      o' \,=./ `o   o' \,=./ `o   .  .:::.        '\\-//`       __MMM__
+    (o o)         (o o)         (o o)         -  (O o)  -   #  (o o)         (o o)         (o o)        :(o o):  .     (o o)         (o o)
+ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--`o'--Ooo----ooO--(_)--Ooo--8---(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+\n\n"""
+print """
+.__   __.  _______ .___________.   .______     ______    __       __       __  .__   __.   _______
+|  \ |  | |   ____||           |   |   _  \   /  __  \  |  |     |  |     |  | |  \ |  |  /  _____|
+|   \|  | |  |__   `---|  |----`   |  |_)  | |  |  |  | |  |     |  |     |  | |   \|  | |  |  __
+|  . `  | |   __|      |  |        |   ___/  |  |  |  | |  |     |  |     |  | |  . `  | |  | |_ |
+|  |\   | |  |____     |  |        |  |      |  `--'  | |  `----.|  `----.|  | |  |\   | |  |__| |
+|__| \__| |_______|    |__|        | _|       \______/  |_______||_______||__| |__| \__|  \______|
+
+\n\n"""
+print "\n\nThe Net Polling server run on the http://"+ip+":4242\n\nWhy 4242?\nBecause 42, it's the Big Question of Life, the Universe and Everything"
+os.popen("python "+path+" manage.py runserver "+str(ip)+":4242 >> "+path+"netpolling.log")
