78de7ed8c595161099460c945bd878288f6f195c
damien <damdam.gold@gmail.com>
2013-09-24 22:35:07 -0700

    Update install of the install script

--- install.py
+++ install.py
@@ -1,45 +1,64 @@
+
 import os
 import re
+import sys
 import ConfigParser
 
 def check_param(obj):
     while 42:
-        value = raw_input(obj.query)
-        if re.search(obj.regex, value):
+        value = raw_input(obj["query"])
+        if re.search(obj["regex"], value):
             return value
         else:
-            print obj.error
+            print obj["error"]
 
 path = "/opt/netpolling/"
-python = ["wget www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz", "tar -xzvf Python-2.7.5.tgz", "cd Python-2.7.5/", "./configure", "make", "make altinstall", "cd .."]
 apt = ["nmap", "python-scapy", "tcpdump", "arp-scan", "tshark"]
-django = ["wget https://www.djangoproject.com/download/1.5.1/tarball/", "tar xzvf Django-1.5.1.tar.gz", "cd Django-1.5.1", "python setup.py install", "cd .."]
-clean = ["Python-2.7.5.tgz", "Django-1.5.1.tar.gz"]
-conf = path+"netpolling/conf/"
-i = 0
+conf = path+"iw/conf/"
 
-os.popen("cd /opt/")
-
-for i in python:
-    os.popen(i)
+print("Installation des dépendances")
 
 for i in apt:
     os.popen("apt-get install "+i+" -y")
 
-for i in django:
-    os.popen(i)
+where = (os.popen("pwd").readlines())[0].replace("\n", "")
 
-for i in clean:
-    os.popen("rm -rf "+i)
+print("Vérification de l'emplace de l'application")
 
+if where == "/opt/netpolling/":
+    pass
+else:
+    print """[ERROR] the Net Polling app must be located in the /opt, with the architecture :
+    /opt/
+        netpolling/
+                    change_password.py
+                    install.py
+                    laucnh.py
+                    manage.py
+                    netpolling/"""
+    sys.exit(0)
+
+print("Préparation du fichier de configuration")
 name = raw_input("Nom du scan : ")
-netmask = {"value":"", "query":"Veuillez fournir le masque réseau (ex : 10.8.96.0/20) : ", "error":"Il faut un masque réseau au format suivant : 10.8.96.0/20", "regex":"^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"}
+netmask = {"value": "", "query": "Veuillez fournir le masque réseau (ex : 10.8.96.0/20) : ", "error": "Il faut un masque réseau au format suivant : 10.8.96.0/20", "regex": "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"}
 interface1 = raw_input("Veuillez indiquer l'interface réseau de scan : ")
 interface2 = raw_input("Veuillez indiquer l'interface réseau d'analyse de bande passante : ")
 bw = {"value":0, "query":"Veuillez indiquer la bande passante (en mo) dédiée au réseau : ", "error":"Votre bande passante doit être un nombre", "regex":"^[0-9]{1,}$"}
-netmask.value = check_param(netmask)
-bw.value = check_param(bw)
-os.popen("cd /opt/netpolling/")
+netmask["value"] = check_param(netmask)
+bw["value"] = check_param(bw)
+print("Ecriture du fichier de configuration")
+config = ConfigParser.RawConfigParser()
+config.read(r''+conf+'netpolling.conf')
+try:
+    config.set("LocalScan", "name", name)
+    config.set("LocalScan", "netmask", netmask["value"])
+    config.set("LocalScan", "interface", interface1)
+    config.set("Listen", "interface", interface2)
+    config.set("Listen", "bw", bw["value"])
+    with open(r''+conf+'netpolling.conf', 'wb') as configfile:
+        config.write(configfile)
+except ConfigParser.Error, err:
+    print 'Oops, une erreur dans votre fichier de conf (%s)' % err
 print "Initialisation de la crontab"
 os.popen("perl /opt/netpolling/netpolling/iw/scripts/cron.pl")
 print "Mise en place des droit Unix"
